4542f0ff3268ab8635552c549d98775541a26718
John Million <jpmillion@comcast.net>
2020-11-23 22:10:16 +0000

    Automatically backed up by Learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -9,7 +9,7 @@ module Players
   class Computer < Player
     
     def move(board)
-      ['5', '1', '3', '7', '9', '2', '4', '6', '8'].detect {|n| board.cells[n-1] == ' '}
+      ['5', '1', '3', '7', '9', '2', '4', '6', '8'].detect {|n| board.cells[n-1] == ' '}.to_s
     end
     
   end
